f2c35971f37b8ddc068469d9a271ca27ab76f9ff
dodyirawan85 <dev.irawans@gmail.com>
2024-02-09 06:52:31 +0700

    X678B: Define screen density
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,5 +9,8 @@ include device/transsion/mt6789-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/infinix/X678B
 
+# Display
+TARGET_SCREEN_DENSITY := 395
+
 # Inherit the proprietary files
 include vendor/infinix/X678B/BoardConfigVendor.mk
