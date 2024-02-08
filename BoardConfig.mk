001d83fb1e95ffca2639a54688ad4163657c1da8
dodyirawan85 <dev.irawans@gmail.com>
2024-02-09 06:51:23 +0700

    X678B: Inherit configuration from common trees
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Include the common OEM chipset BoardConfig.
+include device/transsion/mt6789-common/BoardConfigCommon.mk
+
 DEVICE_PATH := device/infinix/X678B
 
 # Inherit the proprietary files
